e043edf362c0b3b0f513dc2626fa7a5ab58f0522
Shivanshudeveloper <developer.shivanshu@gmail.com>
2020-04-03 09:00:16 +0530

    PLEASE WORK 2

--- app.js
+++ app.js
@@ -11,37 +11,27 @@ app.use(express.json());
 
 
 app.use('/', (req, res) => {
-    const output = `
-                <h1>Graphic Era Deemed to be University Goods Request</h1>
-                <h3>Details:</h3>
-                
-                <a href="https://management-cloud-geu.azurewebsites.net/">Submit Your Quataion</a>
-                <p>
-                    Regards,
-                    'sds'
-                    Mob: '32323'
-                </p>
-                h3>In case of any query please contact to the Adminstration</h3>
-    `;
-    // create reusable transporter object using the default SMTP transport
+    // Node Mailer
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'akhilnegigeu@gmail.com',
-            pass: 'work@1234'
-        }    
+            user: 'shivanshu981@gmail.com',
+            pass: 'ironman1234'
+        }
     });
-    // send mail with defined transport object
-    let info = transporter.sendMail({
-        from: 'akhilnegigeu@gmail.com', // sender address
-        to: 'shivanshu981@gmail.com', // list of receivers
-        subject: `Quatation Required for`, // Subject line
-        text: "Goods Request", // plain text body
-        html: output // html body
-    }).then(() => {
-        res.send(`SEND`);
+
+    let mailOption = {
+        from: 'shivanshu981@gmail.com',
+        to: 'akhilnegigeu@gmail.com',
+        subject: 'Testing and Testing',
+        text: 'IT works'
+    };
+
+    transporter.sendMail(mailOption, (err, data) => {
+        if (err) throw err;
+
+        console.log('Email Sent!');
     })
-    .catch(err => res.send(err));
 });
 
 // Getting PORT set
