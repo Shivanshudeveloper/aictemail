e5133926a057d66f53f93e5ef93fea3747af2351
Shivanshu981 <developer.shivanshu@gmail.com>
2020-06-23 22:31:03 -0700

    IKS Mail

--- app.js
+++ app.js
@@ -196,6 +196,49 @@ app.get('/mtranslation/:email/:course/:language', (req, res) => {
 });
 
 
+app.get('/iksexpert/:email/:fullname', (req, res) => {
+    const email = req.params.email;
+    const fullname = req.params.fullname;
+    console.log(fullname);
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'iksaicte@gmail.com',
+            pass: 'aicte1234'
+        }
+    });
+    let mailOption = {
+        from: 'iksaicte@gmail.com',
+        to: email,
+        subject: `Indian Knowladge System`,
+        text: `
+            Dear Shri/Smt. ${fullname}
+
+            Honorable Minister for Human Resource Development (MHRD) Shri Ramesh Pokhryal "Nishank" had initiated a brainstorming session of experts in the domain of Indian Traditional Knowledge Systems on 18th March 2020. The meeting was Chaired by the HRM and went on for nearly 6 hours.
+            There are some who could not be contacted or could not attend the meeting due to pre occupation. This mail is for informing all of you about the meeting and future course of action.
+            An "Indian Knowledge Systems" Division is created in MHRD which shall be operated from AICTE office in Vasant Kunj.
+            A portal has been created, where you can upload the research work you have already undertaken in the Indian Knowledge Systems, present ongoing work, your commitment to future course of action, especially further research work that you would be interested in undertaking in IKS, helping in providing literature for creating knowledge database in IKS for books, further research, willingness to be part of some IKS knowledge groups so as to start the work in IKS domains in right earnest.
+            Your name was suggested by some experts stating that you too are interested in this domain. Hence, we request you to give information not only about you and also give a list of other experts working in the IKS domains about whom you know so that our database of experts can be expanded which will be useful in moving ahead.
+            IKS website live url :  http://iks.aicte-india.org/
+            In order to promote interdisciplinary research on all aspects of Indian Knowledge Systems, preserve, and disseminate Indian Knowledge Systems for further research and societal applications, a database of individuals and organizations (both public and private) who have contributed by way of Research, Teaching, Publication and Preservation of ancient and contemporary rich Indian Knowledge Systems ranging from art, music, dance, drama, mathematics, astronomy, science, technology, life sciences, environment and natural sciences, health care, yoga, law, jurisprudence, economics, social sciences, psychology, philosophy, management, linguistics, oral traditions of India, panchanga, jyotish shastra, knowledge hidden in Sanskrit, Prakrit, Tamil, Pali etc is necessary. Further, all such collated information needs to be scrutinized, verified, and put in context through interdisciplinary scientific investigation; create a portal for archival and dissemination of this rich knowledge. Hence, we appeal to you to share your interests on this portal for further processing and meet the above objectives.
+            Once, various committees are set up, we will start regular online meetings and discuss issues related to IKS.
+            The Honourable Minister, HRD will also hold review meetings at regular intervals to promote, support and inspire all of us. 
+            AICTE has already sanctioned a project to IIM Bangalore coordinated by Prof. Mahadevan for writing an authentic book on Indian Knowledge System.
+            Together, we can make India proud of its rich cultural and scientific heritage and knowledge and continue further research work in various domains and bring out applications for the benefit of the society.
+
+            Regards,
+            Anil Sahasrabudhe
+            Chairman AICTE
+        `
+    };
+    transporter.sendMail(mailOption, (err, data) => {
+        if (err) throw res.send(err);
+        console.log('Email Sent!');
+        res.send("D");
+    })
+});
+
+
 
 // Getting PORT set
 const PORT = process.env.PORT || 5000;
